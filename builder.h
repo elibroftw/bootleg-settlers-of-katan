3c2521b0f7d0d11928706b4e0fb0b0392a7a6253
Abdullah Hadi <abdullah.hadi350@gmail.com>
2020-12-01 15:45:34 -0500

    Added builder header file

---
+++ builder.h
@@ -0,0 +1,30 @@
+#ifndef __BUILDER__
+#define __BUILDER__
+#include <string>
+#include <vector>
+
+class Builder {
+    // indicate colour: red, blue, orange, yellow
+    std::string colour;
+    // store the resources the builder has: brick, energy, glass,
+    // heat, wifi
+    std::vector<int> resources;
+    // store number of points the builder has
+    int buildPoints;
+    public: 
+        // When a seven is rolled, current builder moves the geese
+        // to any tile. Any builder with 10 or more resources will
+        // automatically lose half their resources. Resources lost 
+        // are chosen randomly.
+        void geeseAttack();
+        // the current builder (who moved the geese), can steal
+        // a random resources from one builder who has a residence
+        // on the tile to where the geese are moved.
+        bool tryStealing();
+        // check if builder won the game, which occurs when they 
+        // have 10 points
+        bool hasWon();
+};
+
+
+#endif
