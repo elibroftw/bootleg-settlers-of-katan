070c84880a26db4efe284f1ffbb2813d45c10009
Elijah Lopez <elijahllopezz@gmail.com>
2020-12-01 23:29:15 -0500

    update builder.h

--- builder.h
+++ builder.h
@@ -1,5 +1,5 @@
-#ifndef __BUILDER__
-#define __BUILDER__
+#ifndef __BUILDER_H__
+#define __BUILDER_H__
 #include <string>
 #include <vector>
 
@@ -11,17 +11,18 @@ class Builder {
     std::vector<int> resources;
     // store number of points the builder has
     int buildPoints;
-    public: 
+    public:
+        Builder(std::string colour);
         // When a seven is rolled, current builder moves the geese
         // to any tile. Any builder with 10 or more resources will
-        // automatically lose half their resources. Resources lost 
+        // automatically lose half their resources. Resources lost
         // are chosen randomly.
         void geeseAttack();
         // the current builder (who moved the geese), can steal
         // a random resources from one builder who has a residence
         // on the tile to where the geese are moved.
         bool tryStealing();
-        // check if builder won the game, which occurs when they 
+        // check if builder won the game, which occurs when they
         // have 10 points
         bool hasWon();
 };
