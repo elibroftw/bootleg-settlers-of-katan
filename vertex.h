430e7bceeb91a01bf409fd7f6519fd7c5fcd71fa
Abdullah Hadi <abdullah.hadi350@gmail.com>
2020-12-02 00:17:08 -0500

    Added header file for vertex

---
+++ vertex.h
@@ -0,0 +1,24 @@
+#ifndef __VERTEX__
+#define __VERTEX__
+#include <string>
+
+class Vertex {
+    int owner;
+    int number;
+    // the x and y coordinates will be used for text display
+    int xCoord;
+    int yCoord;
+    std::string improvement;
+    public:
+        // turns a vertex into a basement,
+        // gives the owner one building point.
+        void addBasement();
+        // If the current residence is a basement, upgrades to a house.
+        // If the current residence is a house, upgrades to a tower.
+        // Otherwise cannot upgrade the residence (must at least be a basement
+        // and at most a tower).
+        // Increases building point by one each time it is called. 
+        void upgradeResidence();
+};
+
+#endif
