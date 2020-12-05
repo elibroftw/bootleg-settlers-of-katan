3e5db6b03b05491ab7b70ee484de0b001404955b
Abdullah Hadi <abdullah.hadi350@gmail.com>
2020-12-04 19:11:17 -0500

    implemented vertex.cc (tentative)

--- vertex.cc
+++ vertex.cc
@@ -22,6 +22,51 @@ int Vertex::getOwner() {
     return owner;
 }
 
-void Vertex::addBasement(shared_ptr<Builder> builder, bool checkResources) {
-
+bool Vertex::upgradeResidence(shared_ptr<Builder> builder, bool checkResources) {
+    std::string errorMsg = "You do not have enough resources.";
+    int numBrick = builder->getResource("brick");
+    int numEnergy = builder->getResource("energy");
+    int numGlass = builder->getResource("glass");
+    int numHeat = builder->getResource("heat");
+    int numWifi = builder->getResource("wifi");
+    if (improvement == "") {
+        if (numBrick >= 1 && numEnergy >= 1 && numGlass >= 1 && numWifi >= 1) {
+            builder->IncreasePoint();
+            builder->setResource("brick", numBrick - 1);
+            builder->setResource("energy", numEnergy - 1);
+            builder->setResource("glass", numGlass - 1);
+            builder->setResource("wifi", numWifi - 1);
+            return true;
+        } else {
+            std::cout << errorMsg << std::endl;
+            return false;
+        }
+    } else if (improvement == "B") {
+        if (numGlass >= 2 && numHeat >= 3) {
+            builder->IncreasePoint();
+            builder->setResource("glass", numGlass - 2);
+            builder->setResource("heat", numHeat - 1);
+            return true;
+        } else {
+            std::cout << errorMsg << std::endl;
+            return false;
+        }
+    } else if (improvement == "H") {
+        if (numBrick >= 3 && numEnergy >= 2 && numGlass >= 2 && numWifi >= 1 && numHeat >= 2) {
+            builder->IncreasePoint();
+            builder->setResource("brick", numBrick - 3);
+            builder->setResource("energy", numEnergy - 2);
+            builder->setResource("glass", numGlass - 2);
+            builder->setResource("wifi", numWifi - 2);
+            builder->setResource("heat", numHeat - 2);
+            return true;
+        } else {
+            std::cout << errorMsg << std::endl;
+            return false;
+        }
+    } else { //improvement is a tower
+        return false;
+    }
 }
+
+
