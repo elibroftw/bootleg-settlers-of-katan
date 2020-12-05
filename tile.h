19107c84a32b049fc69532597417d65d51160301
Elijah Lopez <elijahllopezz@gmail.com>
2020-12-05 00:49:17 -0500

    Update tile.h

--- tile.h
+++ tile.h
@@ -1,18 +1,14 @@
 #ifndef __TILE__
 #define __TILE__
 #include <string>
-#include <unordered_map>
 
 using std::string;
-using std::unordered_map;
 
 class Tile {
     unsigned int number;
     unsigned int value;
     string resource;
-    unordered_map<std::string, Vertex> vertices;
     bool geese;
-    unordered_map<std::string, Edge> edges;
     public:
         string getResource();
         int getNumber();
